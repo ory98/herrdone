271a6e011f63900ad2d2777a5e68739f754212d4
jinsubin <jinsubin8360@gmail.com>
2023-05-27 16:59:00 +0900

    project init

---
+++ src/main/java/com/example/herrdone/HerrdoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.herrdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerrdoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerrdoneApplication.class, args);
+	}
+
+}
