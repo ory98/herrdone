b6edc924397ab3d65141ff90153869bf4c4ecfbf
rlaxotjs2745 <taeseon1997@gmail.com>
2023-06-22 16:56:06 +0900

    modify AuthAspect

--- src/main/java/com/example/herrdone/config/aop/AuthAspect.java
+++ src/main/java/com/example/herrdone/config/aop/AuthAspect.java
@@ -26,17 +26,17 @@ public class AuthAspect {
     @Before("@annotation(com.example.herrdone.config.security.annotation.AuthCheck)")
     public void vaildMember() {
 
-        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
-        HttpServletRequest request = ((ServletRequestAttributes)requestAttributes).getRequest();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes(); // 현재 온 요청 객체를 불러옴
+        HttpServletRequest request = ((ServletRequestAttributes)requestAttributes).getRequest(); // 그 요청 객체에서 HttpServletRequest 객체를 가져옴
 
-        String token = request.getHeader("Authorization");
-        String emailCheck = jwtManager.getMemberEmail(token);
+        String token = request.getHeader("Authorization"); // HttpServletRequest 객체에서 토큰을 할당한 Authorization의 값을 가져옴
+        String emailCheck = jwtManager.getMemberEmail(token); // 토큰으로 이메일 값을 뽑아냄
 
-        if(!memberRepository.existsMemberByEmail(emailCheck)){
-            throw new BusinessException(ErrorCode.INVALID_TOKEN);
+        if(!memberRepository.existsMemberByEmail(emailCheck)){ // 이메일을 DB에서 찾아봤는데 없으면
+            throw new BusinessException(ErrorCode.INVALID_TOKEN); // 에러 내보냄
         }
 
-        request.setAttribute("email", emailCheck);
+        request.setAttribute("email", emailCheck); // 만약에 찾아봤는데 있으면 HttpServletRequest 객체에 email이라는 키값을 만들어서 거기에 할당해줌
     }
 
 }
