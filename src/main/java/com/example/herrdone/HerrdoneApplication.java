991814728776609bde8385466fbe9e783fbe6369
rlaxotjs2745 <taeseon1997@gmail.com>
2023-06-26 08:59:45 +0900

    decode pw encoding

--- src/main/java/com/example/herrdone/HerrdoneApplication.java
+++ src/main/java/com/example/herrdone/HerrdoneApplication.java
@@ -3,12 +3,15 @@ package com.example.herrdone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class HerrdoneApplication {
 
 	public static void main(String[] args) {
+		PasswordEncoder passwordEncoder = null;
+		System.out.println(passwordEncoder.encode("123123"));
 		SpringApplication.run(HerrdoneApplication.class, args);
 	}
 
